6203dfd13d9c0c4402fd1112a5afa3b9dc9f29aa
Netsplits <netsplits@yahoo.es>
2021-07-16 22:11:20 +0200

    Edit to permit change schedule after task inited.

--- src/TaskTypes.cpp
+++ src/TaskTypes.cpp
@@ -38,14 +38,13 @@ TimerTask::TimerTask() : callback() {
 }
 
 void TimerTask::initialise(sched_t when, TimerUnit unit, TimerFn execCallback, bool repeating) {
+    tm_internal::atomicWritePtr(&next, nullptr);
     handleScheduling(when, unit, repeating);
     this->callback = execCallback;
     this->executeMode = EXECTYPE_FUNCTION;
 }
 
 void TimerTask::handleScheduling(sched_t when, TimerUnit unit, bool repeating) {
-    tm_internal::atomicWritePtr(&next, nullptr);
-
     if(unit == TIME_SECONDS) {
         when = when * sched_t(1000);
         unit = TIME_MILLIS;
@@ -56,12 +55,14 @@ void TimerTask::handleScheduling(sched_t when, TimerUnit unit, bool repeating) {
 }
 
 void TimerTask::initialise(uint32_t when, TimerUnit unit, Executable* execCallback, bool deleteWhenDone, bool repeating) {
+    tm_internal::atomicWritePtr(&next, nullptr);
     handleScheduling(when, unit, repeating);
     this->taskRef = execCallback;
     this->executeMode = deleteWhenDone ? ExecutionType(EXECTYPE_EXECUTABLE | EXECTYPE_DELETE_ON_DONE) : EXECTYPE_EXECUTABLE;
 }
 
 void TimerTask::initialiseEvent(BaseEvent* event, bool deleteWhenDone) {
+    tm_internal::atomicWritePtr(&next, nullptr);
     handleScheduling(0, TIME_MICROS, true);
     this->eventRef = event;
     this->executeMode = deleteWhenDone ? ExecutionType(EXECTYPE_EVENT | EXECTYPE_DELETE_ON_DONE) : EXECTYPE_EVENT;
